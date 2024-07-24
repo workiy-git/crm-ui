ca65e02edb49a5d2ca29a1cd9100bfc1984d99c9
workiy-git <ponkaren.raj@workiy.ca>
2024-07-24 10:48:07 +0530

    UI not reflected
    UI not reflected

--- src/components/atoms/search.jsx
+++ src/components/atoms/search.jsx
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Container, Grid, Paper, MenuItem, TextField, Button } from '@mui/material';
 import config from '../../config/config'; // Import the configuration file
+import SearchIcon from '@mui/icons-material/Search';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
 
 const Search = () => {
   const [menuData, setMenuData] = useState([]);
   const [isSearchOpen, setIsSearchOpen] = useState(false); // State to control search container visibility
+  const [searchValue, setSearchValue] = useState('');
+  const [searchResult, setSearchResult] = useState(null);
 
   useEffect(() => {
     axios.get(`${config.apiUrl}/menus/menu_bar`) // Use apiUrl from the configuration file
@@ -20,59 +30,109 @@ const Search = () => {
   }, []);
 
   const handleSearchClick = () => {
-    setIsSearchOpen(prevState => !prevState); // Toggle the value of isSearchOpen
+    setIsSearchOpen(true);
   };
 
-  const handleCloseSearch = () => {
+  const handleSearchClose = () => {
     setIsSearchOpen(false);
+    setSearchResult(null); // Reset search result when closing
   };
+
+  const handleSearchInputChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    axios.get(`${config.apiUrl}/appdata`, { params: { query: searchValue } })
+      .then((response) => {
+        setSearchResult(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching search data:', error);
+        setSearchResult(null); // Reset search result in case of an error
+      });
+  };
+
+  const SearchContainer = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    width: '100%',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
 
   return (
-    <Container style={{padding:'10px'}}>
-      <Grid container spacing={2}>
-        {Array.isArray(menuData) && menuData.map((menuItem, index) => (
-          <Grid item key={index}>
-            {/* Assuming menuItem.search_icon.icon is the URL of the image */}
-            {/* <img
-              src={menuItem.search_icon.icon}
-              alt='icon'
-              style={{ width: '30px', height: 'auto', cursor: 'pointer' }} // Adjust styling as needed
-              onClick={handleSearchClick}
-            /> */}
-            <div>hiuhj</div>
-          </Grid>
-        ))}
-      </Grid>
-      {isSearchOpen && (
-        <Paper style={{ padding: '20px', position:'absolute', zIndex:'1' }}>
-          {/* <Typography variant="h6">Search</Typography> */}
-          <Container style={{display:'flex'}}>
-            <TextField
-              label="Search"
-              fullWidth
-              variant="outlined"
-              style={{ marginTop: '10px' }}
-            />
-            <TextField
-              select
-              label="Dropdown"
-              fullWidth
-              variant="outlined"
-              style={{ marginTop: '10px' }}
-            >
-              <MenuItem value={1}>Option 1</MenuItem>
-              <MenuItem value={2}>Option 2</MenuItem>
-              <MenuItem value={3}>Option 3</MenuItem>
-            </TextField>
-          </Container>
-          <Grid container justifyContent="flex-end" spacing={2} style={{ marginTop: '10px' }}>
-            <Grid item>
-              <Button variant="contained" onClick={handleCloseSearch}>Close</Button>
-            </Grid>
-          </Grid>
-        </Paper>
-      )}
-    </Container>
+    <div>
+      <SearchContainer>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search…"
+          inputProps={{ 'aria-label': 'search' }}
+          onClick={handleSearchClick}
+        />
+      </SearchContainer>
+
+      <Dialog open={isSearchOpen} onClose={handleSearchClose}>
+        <DialogTitle>Search</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Enter search value"
+            fullWidth
+            variant="standard"
+            value={searchValue}
+            onChange={handleSearchInputChange}
+          />
+          {searchResult && (
+            <div>
+              <h4>Search Results:</h4>
+              <pre>{JSON.stringify(searchResult, null, 2)}</pre>
+            </div>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleSearchClose}>Cancel</Button>
+          <Button onClick={handleSearchSubmit}>Search</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 };
 
