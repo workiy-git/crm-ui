b17f452222d149f76053248d6015683c0ce08314
workiy-git <ponkaren.raj@workiy.ca>
2024-09-02 20:51:23 +0530

    Worked on bug fixing
    Worked on bug fixing

--- src/components/atoms/slide_button.jsx
+++ src/components/atoms/slide_button.jsx
@@ -9,12 +9,12 @@ const SlideButton = ({ onScrollLeft, onScrollRight, direction }) => {
       <div>
           {renderLeftArrow && (
             <button onClick={onScrollLeft} style={{ border: 'none', background: 'none' }}>
-              <img src="https://workiy-crm-images.s3.us-east-2.amazonaws.com/Menu/arrow_left.png" alt="Left Arrow" style={{ width: '40px', height: '40px' }} />
+              <img src="https://workiy-crm-images.s3.us-east-2.amazonaws.com/Menu/arrow_left.png" alt="Left Arrow" style={{ width: '40px', height: '40px', cursor:'pointer' }} />
             </button>
           )}
           {renderRightArrow && (
             <button onClick={onScrollRight} style={{ border: 'none', background: 'none' }}>
-              <img src="https://workiy-crm-images.s3.us-east-2.amazonaws.com/Menu/arrow_right.png" alt="Right Arrow" style={{ width: '40px', height: '40px' }} />
+              <img src="https://workiy-crm-images.s3.us-east-2.amazonaws.com/Menu/arrow_right.png" alt="Right Arrow" style={{ width: '40px', height: '40px', cursor:'pointer' }} />
             </button>
           )}
       </div>
