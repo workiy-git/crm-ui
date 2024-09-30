1f5dbc32682cbfa2b5ee5363dbccdf735d9f7a1e
workiy-git <101689935+workiy-git@users.noreply.github.com>
2024-09-30 13:19:05 +0530

    Update config.production.js

--- src/config/config.production.js
+++ src/config/config.production.js
@@ -1,6 +1,6 @@
 // config.production.js
 const prodConfig = {
-  apiUrl: "https://2ri4gekir4.execute-api.ap-south-1.amazonaws.com/dev",
+  apiUrl: "https://2ri4gekir4.execute-api.ap-south-1.amazonaws.com/dev/api",
   // other production configurations...
 };
 
