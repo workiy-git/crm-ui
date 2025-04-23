2bef6611221b7bd4e773299be8de2361132892d0
workiy-git <101689935+workiy-git@users.noreply.github.com>
2025-04-23 14:48:40 +0530

    Update config.staging.js

--- src/config/config.staging.js
+++ src/config/config.staging.js
@@ -1,6 +1,6 @@
 // config.Staging.js
 const stageConfig = {
-  apiUrl: "https://6g35yrrz56.execute-api.ap-south-1.amazonaws.com/crm-server-staging/api",
+  apiUrl: "https://2ri4gekir4.execute-api.ap-south-1.amazonaws.com/dev/api",
 };
 
 export default stageConfig;
