3352ba6f004abe5e9b37b81721cc6e3ffd24b104
Sathish Deivasigamani <137150928+Sathish-Deivasigamani@users.noreply.github.com>
2025-06-18 10:25:28 +0530

    worked on edit page
    worked on edit page

--- src/components/organism/add.jsx
+++ src/components/organism/add.jsx
@@ -216,7 +216,7 @@ const AddComponent = forwardRef(({ formData, setFormData, pageSchema, onSaveSucc
       console.log("formattedLocalValue", formattedLocalValue)
 
       console.log("local", localValue)
-
+      
   
       return (
         <FormControl
@@ -254,7 +254,7 @@ const AddComponent = forwardRef(({ formData, setFormData, pageSchema, onSaveSucc
           <FormControl className='details_page_inputs' key={field.fieldName} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '50%' }} error={!!isError}>
             <label style={{ width: '40%', textAlign: 'left' }}>{label}</label>
             <div style={{ width: '50%'}}>
-            <Select style={{width:'100% !important'}} className='edit-field-input' {...commonProps} displayEmpty>
+            <Select disabled={field.display === 'disable' || field.display === 'none'} style={{width:'100% !important'}} className='edit-field-input' {...commonProps} displayEmpty>
               <MenuItem value="">
                 <em>{field.placeholder || 'Select an option'}</em>
               </MenuItem>
